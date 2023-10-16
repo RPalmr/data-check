cf1903531052d0c59e6f868380c19d629b5f9524
reece palmer <reecelpalmer@gmail.com>
2023-10-16 11:44:08 +0100

    Solved the first challenge of the bootcamp :)

--- demo.py
+++ demo.py
@@ -4,4 +4,5 @@ def circle_area(radius):
     """Returns the area of the circle of given radius"""
     if radius < 0:
         return 0
-    return math.pi * radius ** 2
+    else:
+        return math.pi * radius ** 2
